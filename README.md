d9f1091234d2ea18158e634e4dfa57cfcdc53271
Dan Schulte <dan.schulte.qub@live.com>
2019-12-05 08:40:29 -0800

    Extract qub-java-json to its own package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Qub for Java - JSON
+
+This repository contains a collection of classes and functions for Java that help when interacting with JSON content.
---
+++ sources/qub/JSONTokenType.java
@@ -0,0 +1,20 @@
+package qub;
+
+public enum JSONTokenType
+{
+    LeftCurlyBracket,
+    RightCurlyBracket,
+    LeftSquareBracket,
+    RightSquareBracket,
+    Colon,
+    Comma,
+    Null,
+    NewLine,
+    QuotedString,
+    Boolean,
+    Number,
+    Whitespace,
+    LineComment,
+    BlockComment,
+    Unrecognized
+}
